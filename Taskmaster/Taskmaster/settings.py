075c8c01ab024f86ea4112a035919e0d4fb45293
Daniel Beckett <dan-beckett@hotmail.co.uk>
2025-01-07 13:17:27 +0000

    Add crispy forms and update Bootstrap to version 5

--- Taskmaster/Taskmaster/settings.py
+++ Taskmaster/Taskmaster/settings.py
@@ -44,6 +44,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'tasks',
+    'crispy_forms',
+    'crispy_bootstrap5',
 ]
 
 MIDDLEWARE = [
@@ -55,6 +57,9 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 ROOT_URLCONF = 'Taskmaster.urls'
 
