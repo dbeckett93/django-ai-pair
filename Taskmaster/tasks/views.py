4947d6c4ccdb657d6be777f945ae56348462eb7f
Daniel Beckett <dan-beckett@hotmail.co.uk>
2025-01-07 12:42:50 +0000

    Sort tasks by due date in ascending order on the index view

--- Taskmaster/tasks/views.py
+++ Taskmaster/tasks/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 from .models import Task
 
 def index(request):
-    tasks_todo = Task.objects.filter(completed=False).order_by('-due_date')
-    tasks_done = Task.objects.filter(completed=True).order_by('-due_date')
+    tasks_todo = Task.objects.filter(completed=False).order_by('due_date')
+    tasks_done = Task.objects.filter(completed=True).order_by('due_date')
     context = {
         'tasks_todo': tasks_todo,
         'tasks_done': tasks_done,
