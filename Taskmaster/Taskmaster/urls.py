5ac9e3c3c7cc78bac51fc999c2548be518d12f22
Daniel Beckett <dan-beckett@hotmail.co.uk>
2025-01-07 12:38:24 +0000

    Implement homepage with task lists and styling

--- Taskmaster/Taskmaster/urls.py
+++ Taskmaster/Taskmaster/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from tasks import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.index, name='index'),  # Homepage URL
     path('tasks/', include('tasks.urls')),
 ]
--- Taskmaster/tasks/views.py
+++ Taskmaster/tasks/views.py
@@ -1,4 +1,11 @@
-from django.http import HttpResponse
+from django.shortcuts import render
+from .models import Task
 
 def index(request):
-    return HttpResponse("Hello, this is the tasks app!")
+    tasks_todo = Task.objects.filter(completed=False).order_by('-due_date')
+    tasks_done = Task.objects.filter(completed=True).order_by('-due_date')
+    context = {
+        'tasks_todo': tasks_todo,
+        'tasks_done': tasks_done,
+    }
+    return render(request, 'tasks/index.html', context)
